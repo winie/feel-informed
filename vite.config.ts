681ecb700cfeed52ad6dd7b241519b4deb4053a2
winie <rolingdx@yahoo.de>
2025-08-21 22:52:48 +0200

    Add action for github page deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Read base from enviroment variable.
+// If it is not provided, ignore it.
+const base = process?.env.BASE ?? undefined;
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: base,
 })
